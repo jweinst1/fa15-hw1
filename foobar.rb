292870794c6837dde37d6d420f0e9b78b802477d
Josh Weinstein <jweinst1@berkeley.edu>
2016-04-26 17:15:52 -0700

    Update foobar.rb

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,12 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(array)
+  	new_array = array.map(&:to_i)
+  	new_array = new_array.map{|i| i+2} #add 2 to every element in the array 
+  	new_array.keep_if{|i| i%2==0 and i<10} #keep even and less than 10
+  	new_array = new_array.uniq
+  	sum = 0
+  	new_array.each{|i| sum+= i}
+  	sum
+  end 
 end
